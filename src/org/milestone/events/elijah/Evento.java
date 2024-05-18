abae2b4150a31ee7ac260419e39e3448c075559c
Elijah <gabriele.tosto@outlook.com>
2024-05-18 15:00:00 +0200

    ADD: IEvento e Evento.class
    Creata l'interfaccia e la classe base di Evento.

---
+++ src/org/milestone/events/elijah/Evento.java
@@ -0,0 +1,5 @@
+package org.milestone.events.elijah;
+
+public class Evento {
+
+}
---
+++ src/org/milestone/events/elijah/IEvento.java
@@ -0,0 +1,8 @@
+package org.milestone.events.elijah;
+
+public interface IEvento {
+	
+	public void prenota();
+	public void disdici();
+	
+}
